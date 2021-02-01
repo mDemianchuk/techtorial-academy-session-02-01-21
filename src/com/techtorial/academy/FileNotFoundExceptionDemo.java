031fab699ecae5552515677483b1d87052dbeb55
mdemianchuk <nickdemianchuk@gmail.com>
2021-02-01 16:10:09 -0600

    Rename FileNotFoundDemo to FileNotFoundExceptionDemo

--- src/com/techtorial/academy/FileNotFoundExceptionDemo.java
+++ src/com/techtorial/academy/FileNotFoundExceptionDemo.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class FileNotFoundDemo {
+public class FileNotFoundExceptionDemo {
     public static void main(String[] args) {
         Scanner consoleReader = new Scanner(System.in);
         String fileContents = "";
